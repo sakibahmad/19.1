26a8d8678d1630463cab501e8d74945ffaa19a1b
sakibahmad <sakibsiddiqui21@gmail.com>
2017-08-31 23:27:53 +0530

    19.1
    19.1

---
+++ app/src/main/java/com/example/root/a191/CommonUtilities.java
@@ -0,0 +1,12 @@
+package com.example.root.a191;
+
+/**
+ * Created by root on 12/8/17.
+ */
+
+public class CommonUtilities {
+    public enum SERVICE_TYPE {
+        GET_DATA, ERROR_REPORT
+
+    }
+}
---
+++ app/src/main/java/com/example/root/a191/Network/WebService.java
@@ -0,0 +1,11 @@
+package com.example.root.a191.Network;
+
+import com.example.root.a191.CommonUtilities;
+
+/**
+ * Created by root on 12/8/17.
+ */
+
+public interface WebService {
+    public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
